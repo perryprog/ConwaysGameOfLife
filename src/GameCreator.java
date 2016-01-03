3633fc84711aeaf1ede2646d761fb3b2b3ae7114
perryprog <perry@frasers.org>
2016-01-03 18:27:23 -0500

    Added a JOptionPane interface. (Umm GUI not the other kind...) Very picky, to lazy to add a Try/Catch :-1:

--- src/GameCreator.java
+++ src/GameCreator.java
@@ -2,13 +2,21 @@ import javax.swing.*;
 
 public class GameCreator {
     public static void main(String[] args) {
-        MainBoard mainBoard = new MainBoard(200, 200, 10, false);
+
+        String boardHeight = JOptionPane.showInputDialog(null, "What is the board width?");
+        String boardWidth = JOptionPane.showInputDialog(null, "What is the board height?");
+        String cellSize = JOptionPane.showInputDialog(null, "What is the cell height?");
+        String whiteIsAlive = JOptionPane.showInputDialog(null, "Should white be alive? (true/false)");
+
+
+        MainBoard mainBoard = new MainBoard(Integer.parseInt(boardHeight), Integer.parseInt(boardWidth), Integer.parseInt(cellSize), Boolean.parseBoolean(whiteIsAlive));
+//        MainBoard mainBoard = new MainBoard(200, 200, 10, false);
 
         mainBoard.makeFrames().
-            genPanel().
-            addComponent(new JLabel("SUP DAWG")).
-            init().
-            showFrame();
+                genPanel().
+                addComponent(new JLabel("SUP DAWG")).
+                init().
+                showFrame();
 
         while (true) {
             mainBoard.conwaysCheker();
